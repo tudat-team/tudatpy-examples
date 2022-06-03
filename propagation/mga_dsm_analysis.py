e4944b8533d1b2cce373c55b417bc41f070c8429
Jérémie Gaffarel <jeremie@gaffarel.pro>
2022-06-03 12:40:22 +0200

    Converted updated DSM notebook to py file

--- propagation/mga_dsm_analysis.py
+++ propagation/mga_dsm_analysis.py
@@ -1,39 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Multiple Gravity Assist trajectories & DSM
+"""
 
-# # Multiple Gravity Assist trajectories & DSM
-# 
-# Copyright (c) 2010-2022, Delft University of Technology. All rights reserved. This file is part of the Tudat. Redistribution and use in source and
-# binary forms, with or without modification, are permitted exclusively
-# under the terms of the Modified BSD license. You should have received
-# a copy of the license with this file. If not, please or visit:
-# http://tudat.tudelft.nl/LICENSE.
-# 
-# ## Context
-# 
-# This example demonstrates how Multiple Gravity Assist (MGA) transfer trajectories 
-# with or without Deep Space Maneuvers (DSM) can be simulated.
-#  
-# In addition, this example show how the results, such as partial $\Delta$V's, total $\Delta$V and Time of Flight
-# values can be retrieved from the transfer object.
-# 
-# A complete guide on transfer trajectory design is given on [this page](https://tudat-space.readthedocs.io/en/latest/_src_user_guide/astrodynamics/trajectory_design.html) of tudat user documentation.
+Copyright (c) 2010-2022, Delft University of Technology. All rights reserved. This file is part of the Tudat. Redistribution and use in source and
+binary forms, with or without modification, are permitted exclusively
+under the terms of the Modified BSD license. You should have received
+a copy of the license with this file. If not, please or visit:
+http://tudat.tudelft.nl/LICENSE.
+"""
 
-# ## Import statements
-# 
-# The required import statements are made here, at the very beginning.
-# 
-# Some standard modules are first loaded: numpy and matplotlib.pyplot.
-# 
-# Then, the different modules of tudatpy that will be used are imported.
 
-# In[1]:
+## Context
+"""
 
+This example demonstrates how Multiple Gravity Assist (MGA) transfer trajectories 
+with or without Deep Space Maneuvers (DSM) can be simulated.
+ 
+In addition, this example show how the results, such as partial $\Delta$V's, total $\Delta$V and Time of Flight
+values can be retrieved from the transfer object.
+
+A complete guide on transfer trajectory design is given on [this page](https://tudat-space.readthedocs.io/en/latest/_src_user_guide/astrodynamics/trajectory_design.html) of tudat user documentation.
+"""
+
+## Import statements
+"""
+
+The required import statements are made here, at the very beginning.
+
+Some standard modules are first loaded: numpy and matplotlib.pyplot.
+
+Then, the different modules of tudatpy that will be used are imported.
+"""
 
 # Load standard modules
 import numpy as np
-# make plots interactive
-get_ipython().run_line_magic('matplotlib', 'widget')
 import matplotlib.pyplot as plt
 
 # Load tudatpy modules
@@ -43,21 +42,20 @@ from tudatpy.util import result2array
 from tudatpy.kernel import constants
 
 
-# ## MGA transfer without DSMs
+## MGA transfer without DSMs
 
 # First, let's explore a Multiple Gravity Assist transfer trajectory with no additional Deep Space Maneuvers.
 
-# ### Setup and inputs
-# 
-# A simplified system of bodies suffices for this application, with the Sun as central body. The planets that are visited for a GA are defined in the list `transfer_body_order`.
-# 
-# The departure and arrival orbit can be specified, but they are not mandatory. If not specified, the departure and arrival planets are considered swing-by nodes.
-# Departures and arrivale at the edge of the Sphere Of Indfluence (SOI) of a node can be done by specifying an eccentricity of 0 and an infinite semi-major axis.
-# 
-# In this example, the spacecraft departs from the edge of Earth’s SOI and is inserted into a highly elliptical orbit around Saturn.
+### Setup and inputs
+"""
 
-# In[2]:
+A simplified system of bodies suffices for this application, with the Sun as central body. The planets that are visited for a GA are defined in the list `transfer_body_order`.
 
+The departure and arrival orbit can be specified, but they are not mandatory. If not specified, the departure and arrival planets are considered swing-by nodes.
+Departures and arrival at the edge of the Sphere Of Influence (SOI) of a node can be done by specifying an eccentricity of 0 and an infinite semi-major axis.
+
+In this example, the spacecraft departs from the edge of Earth’s SOI and is inserted into a highly elliptical orbit around Saturn.
+"""
 
 # Create a system of simplified bodies (create all main solar system bodies with their simplest models)
 bodies = environment_setup.create_simplified_system_of_bodies()
@@ -74,13 +72,10 @@ arrival_semi_major_axis = 1.0895e8 / 0.02
 arrival_eccentricity = 0.98
 
 
-# ### Create transfer settings and transfer object
+### Create transfer settings and transfer object
 
 # The specified inputs can not be used directly, but they have to be translated to distinct settings, relating to either the nodes (GAs at planets) or legs (trajectories in between planets). The fact that no DSMs are used is indicated by the creation of unpowered and unperturbed leg settings.
 # These settings are, in turn, used to create the transfer trajectory object.
-
-# In[3]:
-
 
 # Define the trajectory settings for both the legs and at the nodes
 transfer_leg_settings, transfer_node_settings = transfer_trajectory.mga_settings_unpowered_unperturbed_legs(
@@ -97,14 +92,11 @@ transfer_trajectory_object = transfer_trajectory.create_transfer_trajectory(
     central_body )
 
 
-# ### Define transfer parameters
+### Define transfer parameters
 
 # The advantage of having a transfer trajectory object is that it allows for analyzing many different sets of transfer parameters for the defined transfer settings. For this first part without DSMs, the transfer parameters only constitute the times at which the powered GA’s are executed, i.e. at the nodes. Since the magnitude and direction of each  is determined by the planet positions at the node times, there is no need to specify node free parameters.
 # There are no DSMs in between the planets, i.e. on the legs, so there is also no need to specify leg free parameters.
 # However, for compatibility reasons, it is required to define the free parameters as a list containing empty arrays.
-
-# In[4]:
-
 
 # Define times at each node
 julian_day = constants.JULIAN_DAY
@@ -127,26 +119,24 @@ for i in transfer_node_settings:
     node_free_parameters.append( np.zeros(0))
 
 
-# ### Evaluate transfer
+### Evaluate transfer
 
 # The transfer parameters are now used to evaluate the transfer trajectory, which means that the astrodynamical propagation is now performed.
-
-# In[5]:
-
 
 # Evaluate the transfer with given parameters
 transfer_trajectory_object.evaluate( node_times, leg_free_parameters, node_free_parameters )
 
 
-# ### Extract results and plot trajectory
-# Last but not least, with the transfer trajectory computed, we can now analyse how it was carried.
+### Extract results and plot trajectory
+"""
+Last but not least, with the transfer trajectory computed, we can now analyse how it was carried.
+"""
 
-# #### Print results
-# Having evaluated the transfer trajectory, it is now possible to extract various transfer characteristics, such as the $\Delta$V and Time of Flight values.
-# Then, the definition of the transfer parameters can be plotted using the `transfer_trajectory.print_parameter_definitions()` function.
-
-# In[6]:
-
+#### Print results
+"""
+Having evaluated the transfer trajectory, it is now possible to extract various transfer characteristics, such as the $\Delta$V and Time of Flight values.
+Then, the definition of the transfer parameters can be plotted using the `transfer_trajectory.print_parameter_definitions()` function.
+"""
 
 # Print the total DeltaV and time of Flight required for the MGA
 print('Total Delta V of %.3f m/s and total Time of flight of %.3f days\n' %     (transfer_trajectory_object.delta_v, transfer_trajectory_object.time_of_flight / julian_day))
@@ -168,11 +158,10 @@ print("Transfer parameter definitions:")
 transfer_trajectory.print_parameter_definitions(transfer_leg_settings, transfer_node_settings)
 
 
-# #### Plot the transfer
-# The state throughout the transfer can be retrieved from the transfer trajectory object, here at 500 instances per leg, to visualize the transfer.
-
-# In[7]:
-
+#### Plot the transfer
+"""
+The state throughout the transfer can be retrieved from the transfer trajectory object, here at 500 instances per leg, to visualize the transfer.
+"""
 
 # Extract the state history
 state_history = transfer_trajectory_object.states_along_trajectory(500)
@@ -207,16 +196,15 @@ plt.tight_layout()
 plt.show()
 
 
-# ## MGA-DSM transfer
+## MGA-DSM transfer
 
 # This next part of the example now makes use of Deep Space Maneuvers in between the nodes.
 # The general approach is identical to the example without DSMs, with some modifications to the inputs and transfer parameters.
 
-# ### Setup and inputs
-# Again, a simplified system of bodies suffices. In this case transfer to Mercury is considered with GA's at Earth and Venus. As before, the departure and arrival orbits and the central body are also specified.
-
-# In[8]:
-
+### Setup and inputs
+"""
+Again, a simplified system of bodies suffices. In this case transfer to Mercury is considered with GA's at Earth and Venus. As before, the departure and arrival orbits and the central body are also specified.
+"""
 
 # Create simplified bodies
 bodies = environment_setup.create_simplified_system_of_bodies()
@@ -233,12 +221,11 @@ arrival_semi_major_axis = np.inf
 arrival_eccentricity = 0.0
 
 
-# ### Create transfer settings and transfer object
-# The transfer settings are created with departure and arrival orbits at the edge of the SOI of the corresponding bodies. The Sun is again taken as central body for the transfer trajectory object.
-# Since DSMs are used in this example, the settings are created using `mga_settings_dsm_velocity_based_legs`.
-
-# In[9]:
-
+### Create transfer settings and transfer object
+"""
+The transfer settings are created with departure and arrival orbits at the edge of the SOI of the corresponding bodies. The Sun is again taken as central body for the transfer trajectory object.
+Since DSMs are used in this example, the settings are created using `mga_settings_dsm_velocity_based_legs`.
+"""
 
 # Define the MGA transfer settings
 transfer_leg_settings, transfer_node_settings = transfer_trajectory.mga_settings_dsm_velocity_based_legs(
@@ -255,26 +242,25 @@ transfer_trajectory_object = transfer_trajectory.create_transfer_trajectory(
     central_body)
 
 
-# ### Define transfer parameters
-# 
-# The `DSM_velocity_based_leg_type` requires more transfer parameters than when no DSMs are executed. That is, this leg type requires leg free and node free parameters. There is a free parameter for each leg and three free parameters for the departure node and each swingby node.
-# 
-# The leg free parameters represents the Time of flight fraction at which the DSM takes place.
-# 
-# The three node free parameters represent the followings:
-#  * For the departure node:
-#     1. Magnitude of the relative velocity w.r.t. the departure planet after departure.
-#     2. In-plane angle of the relative velocity w.r.t. the departure planet after departure.
-#     3. Out-of-plane angle of the relative velocity w.r.t. the departure planet after departure.
-#  * For the swing-by nodes:
-#     1. Periapsis radius.
-#     2. Rotation angle.
-#     3. Magnitude of $\Delta$V applied at periapsis.
-# 
-# 
+### Define transfer parameters
+"""
 
-# In[10]:
+The `DSM_velocity_based_leg_type` requires more transfer parameters than when no DSMs are executed. That is, this leg type requires leg free and node free parameters. There is a free parameter for each leg and three free parameters for the departure node and each swingby node.
 
+The leg free parameters represents the Time of flight fraction at which the DSM takes place.
+
+The three node free parameters represent the followings:
+ * For the departure node:
+    1. Magnitude of the relative velocity w.r.t. the departure planet after departure.
+    2. In-plane angle of the relative velocity w.r.t. the departure planet after departure.
+    3. Out-of-plane angle of the relative velocity w.r.t. the departure planet after departure.
+ * For the swing-by nodes:
+    1. Periapsis radius.
+    2. Rotation angle.
+    3. Magnitude of $\Delta$V applied at periapsis.
+
+
+"""
 
 # Define times at each node
 julian_day = constants.JULIAN_DAY
@@ -301,24 +287,25 @@ node_free_parameters.append(np.array([1.10000000891 * 6.052e6, 1.34317576594, 0.
 node_free_parameters.append(np.array([]))
 
 
-# ### Evaluate transfer
-# The same approach is used to evaluate the transfer trajectory with the transfer parameters.
-
-# In[11]:
-
+### Evaluate transfer
+"""
+The same approach is used to evaluate the transfer trajectory with the transfer parameters.
+"""
 
 # Evaluate the transfer with the given parameters
 transfer_trajectory_object.evaluate( node_times, leg_free_parameters, node_free_parameters)
 
 
-# ### Extract results and plot trajectory
-# Finally, the results are extracted and used to visualize the transfer trajectory. 
-# 
-# #### Print results
-# Again, the transfer parameter definitions can be printed for reference and are significantly different from the one for the transfer without DSMs.
+### Extract results and plot trajectory
+"""
+Finally, the results are extracted and used to visualize the transfer trajectory. 
+"""
 
-# In[12]:
 
+#### Print results
+"""
+Again, the transfer parameter definitions can be printed for reference and are significantly different from the one for the transfer without DSMs.
+"""
 
 # Print the total DeltaV and time of Flight required for the MGA
 print('Total Delta V of %.3f m/s and total Time of flight of %.3f days\n' %     (transfer_trajectory_object.delta_v, transfer_trajectory_object.time_of_flight / julian_day))
@@ -340,11 +327,10 @@ print("Transfer parameter definitions:")
 transfer_trajectory.print_parameter_definitions(transfer_leg_settings, transfer_node_settings)
 
 
-# #### Plot the transfer
-# The state throughout the transfer can be retrieved from the transfer trajectory object, here at 500 instances per leg, to visualize the transfer.
-
-# In[13]:
-
+#### Plot the transfer
+"""
+The state throughout the transfer can be retrieved from the transfer trajectory object, here at 500 instances per leg, to visualize the transfer.
+"""
 
 # Extract the state history
 state_history = transfer_trajectory_object.states_along_trajectory(500)
@@ -367,9 +353,6 @@ ax.set_ylabel('y wrt Sun [AU]')
 ax.set_aspect('equal')
 ax.legend(bbox_to_anchor=[1, 1])
 plt.show()
-
-
-# In[ ]:
 
 
 
