84ae4db6917191ddea2c754fba86893e3a63efe4
Dominic Dirkx <d.dirkx@tudelft.nl>
2022-06-07 11:34:49 +0200

    Updated to new version

--- propagation/two_stage_rocket_ascent.py
+++ propagation/two_stage_rocket_ascent.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, '/home/dominic/Software/tudat-bundle/build-tudat-bundle-Desktop-Default/tudatpy/')
+
 # Two-stage rocket ascent
 """
 Copyright (c) 2010-2022, Delft University of Technology. All rights reserved. This file is part of the Tudat. Redistribution and use in source and binary forms, with or without modification, are permitted exclusively under the terms of the Modified BSD license. You should have received a copy of the license with this file. If not, please or visit: http://tudat.tudelft.nl/LICENSE.
